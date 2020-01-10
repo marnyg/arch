623ff792104691e12d576cb19d920c2106468ab7
marius <marnyg31.10@gmail.com>
2020-01-10 22:19:13 +0100

    refactor and improved script

--- wrapper.sh
+++ wrapper.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 echo installing git
-pacman -Sy
-pacman -S git
+pacman -Sy --noconfirm
+pacman -S git --noconfirm
 
 echo cloning install files
 git clone https://github.com/marnyg/arch

e5e34f30c8d62e1d4fd1b39e98860a893019f6f5
Marius Nygård <marius@anteo.no>
2021-06-01 16:52:42 +0200

    making a manual version

---
+++ manualMBR/manualInstall.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+update_pacman_mirror(){
+    ## Norway
+    echo "Server = http://mirror.neuf.no/archlinux/\$repo/os/\$arch" | cat - /etc/pacman.d/mirrorlist > temp && mv temp /etc/pacman.d/mirrorlist
+}
+
+timedatectl set-ntp true
+
+update_pacman_mirror
+#packstrap
+pacstrap /mnt base base-devel linux linux-firmware vim
+##GenfsTab
+genfstab -U /mnt >> /mnt/etc/fstab
+cp part2 /mnt/root/part2
+
+arch-chroot /mnt
